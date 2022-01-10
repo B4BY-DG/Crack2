ebe8069afe044d9bda769fd4ae32e5d32f7b1bdd
Sarfraz Ahmed Shar <86257930+Sarfraz-Baloch@users.noreply.github.com>
2022-01-10 14:23:06 +0500

    Create sk.py

---
+++ sk.py
@@ -0,0 +1,17 @@
+#coding=utf-8
+ 
+import os,sys,random,struct
+ 
+x = str(struct.calcsize("P") * 8)
+
+if '64' in x:
+ 
+    os.system('chmod 777 B64 && ./B64')
+ 
+else:
+ 
+    print('\033[1;31m   please change ur phone then try again\033[0;97m')
+    os.system('xdg-open https://facebook.com/groups/1267278830395332/')
+    os.sys.exit()
+ 
+ 
